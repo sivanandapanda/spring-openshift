971e84cb72de7d48da67182d6a263988d42d8130
siva <sivanandpanda@gmail.com>
2021-08-24 20:42:10 +0800

    first commit

---
+++ src/main/java/com/example/springopenshift/SpringOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.springopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenshiftApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/springopenshift/service/FruitRepository.java
@@ -0,0 +1,6 @@
+package com.example.springopenshift.service;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FruitRepository extends CrudRepository<Fruit, Integer> {
+}
